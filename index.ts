2c88e4c0f1711726c932cd5114968b0d09f86d08
spearfish <caowlhs@163.com>
2024-10-24 10:35:45 +0800

    add  routes

--- index.ts
+++ index.ts
@@ -1,9 +1,25 @@
-const z = 10
+import figlet from "figlet";
 
-const read = (text: string) => {
-    return text
-}
+const server = Bun.serve({
+    port: 3000,
+    fetch(req) {
+        const url = new URL(req.url);
+        if (url.pathname === "/") {
+            const body = figlet.textSync("Vid22eo!");
+            return new Response(body);
+        }
+        if (url.pathname === "/about") {
+            return new Response("About me");
+        }
+        return new Response("Home Page");
+    },
+    error(error) {
+        return new Response(`<pre> ${error.stack} </pre>`, {
+             headers: {
+                "Content-Type": "text/html"
+             }
+            });
+    }
+});
 
-const msg = read("Hello world!@")
-
-console.log(msg)
+console.log(`Listening on PORT https://localhost:${server.port}`)
